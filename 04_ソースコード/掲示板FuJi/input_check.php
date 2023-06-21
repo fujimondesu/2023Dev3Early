d92a234ec69c8049a40d5a00fce6f0032369f0f0
yuksaki72 <109458803+yuksaki72@users.noreply.github.com>
2023-06-21 09:24:19 +0900

    修正

--- 04_ソースコード/掲示板FuJi/input_check.php
+++ 04_ソースコード/掲示板FuJi/input_check.php
@@ -1,16 +1,6 @@
 <?php
 session_start();
 
-// $_SESSION['page']      ==  どのページから来たか
-// $_SESSION['user_name'] ==  ユーザー名
-// $_SESSION['pass']      ==  パスワード（新規登録）
-// $_SESSION['mail']      ==  メールアドレス（新規登録）
-// $_SESSION['user_id']   ==  ユーザーid
-// $_SESSION['genre_id']  ==  ジャンルid
-// $_SESSION['room_id']   ==  スレッド（話題）id
-// $_SESSION['error']     ==  エラー文（複数の場合は,でくっつける予定）
-// $_SESSION['chat']      ==  チャット文
-
 // メールアドレスとパスワードが入力されているか
 if(empty($_POST['mail']) || empty($_POST['pass'])) {
     $_SESSION['error'] = "メールアドレスまたはパスワードが入力されていません。";
