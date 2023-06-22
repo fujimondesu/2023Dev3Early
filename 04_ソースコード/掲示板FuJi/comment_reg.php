0a0a9bcaf6ab74cd1ceafad11519d079b180c0a5
yuksaki72 <109458803+yuksaki72@users.noreply.github.com>
2023-06-22 17:32:20 +0900

    コメント追加

--- 04_ソースコード/掲示板FuJi/comment_reg.php
+++ 04_ソースコード/掲示板FuJi/comment_reg.php
@@ -2,6 +2,7 @@
 session_start();
 require_once './DBManager.php';
 $dbmng = new DBManager();
+// チャット文章をデータベースに登録
 $user = $dbmng->chatRegist($_SESSION['user_id'],$_SESSION['room_id'],$_POST['chat']);
 
 header('Location: chat.php');
