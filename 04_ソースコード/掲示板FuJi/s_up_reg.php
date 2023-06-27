e35dd5c91f4094410f46cb46becc8351cf71e051
yuksaki72 <109458803+yuksaki72@users.noreply.github.com>
2023-06-27 09:28:25 +0900

    重複ファイル削除

--- 04_ソースコード/掲示板FuJi/s_up_reg.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-require_once './DBManager.php';
-$dbmng = new DBManager();
-$user = $dbmng->userRegist($_POST['user_name'],$_POST['mail'],$_POST['pass']);
-
-header('Location: su_com.html');
-?>
