b893ad7ead5928c13e6aa23378611eaab0fb0024
yuksaki72 <109458803+yuksaki72@users.noreply.github.com>
2023-06-29 20:10:18 +0900

    完成（要確認）

--- 04_ソースコード/掲示板FuJi/home.php
+++ 04_ソースコード/掲示板FuJi/home.php
@@ -14,14 +14,23 @@ $userName = $dbmng->userNameGet($_SESSION['user_id']);
 $userName = $userName[0][0];
 $genreNames;
 $genreIds;
-$genreLength;
+$threadIds;
+$threadNames;
+$threadDetails;
 $i = 0;
 foreach ($getGenres as $row) {
   $genreIds[$i] = $row['genre_id'];
   $genreNames[$i] = $row['genre_name'];
   $i++;
 }
-$genreLength = count($genreIds);
+
+if (!empty($_POST['genre_id'])) {
+  $_SESSION['genre_id'] = $_POST['genre_id'];
+  var_dump($_SESSION['genre_id']);
+}
+
+// テスト用
+var_dump($_POST);
 ?>
 <!-- ホーム画面(最初ここ) -->
 <!doctype html>
@@ -88,6 +97,14 @@ $genreLength = count($genreIds);
       border-left: 7px solid #f7f7f7;
       border-right: 7px solid #f7f7f7;
       border-bottom: 10px solid #fff;
+    }
+
+    button {
+      position: relative;
+      background: transparent;
+      border-color: transparent transparent transparent transparent;
+      width: 100%;
+      height: 30%;
     }
 
     .info {
@@ -170,7 +187,6 @@ $genreLength = count($genreIds);
         }
         ?>
 
-        <!-- <a href="./logout.php" class="header-R-child-off">logout</a> -->
       </div>
     </div>
   </div>
@@ -185,28 +201,37 @@ $genreLength = count($genreIds);
           <div style="background-color: aqua; height: 50px; display: flex; justify-content: space-between;">
             <div style="width:80%; display: flex; justify-content: center; align-items: center;">
               <?php
-              // echo $userName;
               echo $userName;
               ?>
             </div>
-            <a href="./new_topic.html" style="width:20%;">
-              <i class="bi bi-plus-lg icon-size"></i>
+            <a href="./new_topic.html" style="width:20%;display: table;">
+              <i class="bi bi-plus-lg icon-size" style="display: table-cell;vertical-align: middle;"></i>
             </a>
           </div>
 
-          <div class="list-group list-group-flush border-bottom scrollarea">
+          <div class="list-group list-group-flush border-bottom scrollarea" style="padding:0;">
             <?php
+            $id = 0;
+            $name = '';
             for ($i = 0; $i < count($genreNames); $i++) {
-              // ニュースを選ばせた状態にするための処理
               $chk = '';
-              if ($i <= 0) {
+              // ジャンルを選んでいたか
+              if (!empty($_POST['genre_id'])) {
+                if ($i + 1 == $_POST['genre_id']) {
+                  $chk = 'checked';
+                } else if ($i + 5 == $_POST['genre_id']) {
+                  $chk = 'checked';
+                }
+              } else if ($i <= 0) {
                 $chk = 'checked';
               }
+              $id = $genreIds[$i];
+              $name = $genreNames[$i];
 
               echo '<div class="genres">';
-              echo '<input type="radio"' . $chk . ' name="genreButtons" id="' . $genreIds[$i] . '" onclick="isClicked(event,this,' . $genreIds[$i] . ')">';
-              echo '<label for="' . $genreIds[$i] . '">';
-              echo '<strong class="mb-1">' . $genreNames[$i] . '</strong>';
+              echo '<input type="radio"' . $chk . ' name="genreButtons" id="' . $id . '" value="' . $id . '" onclick="isClicked(event,this,' . $id . ')">';
+              echo '<label for="' . $id . '">';
+              echo '<strong class="mb-1">' . $name . '</strong>';
               echo '</label>';
               echo '</div>';
               $chk = '';
@@ -220,19 +245,7 @@ $genreLength = count($genreIds);
         <!-- 話題一覧 -->
         <div class="viewScroll" id="thread">
 
-          <?php
-          // $getGenres = $dbmng->getThreadList();
-          ?>
-
-          <!-- <section>
-            <article>
-              <div class="info">
-                <h2><?php echo "ユーザー名"; ?></h2>
-                <time><?php echo date('Y年m月d日 H:i', strtotime("2019-03-20 23:22:47"));  ?></time>
-              </div>
-              <p><?php echo "本文"; ?></p>
-            </article>
-          </section> -->
+          <!-- ここにスレッドが入る -->
 
         </div>
       </div>
@@ -241,76 +254,128 @@ $genreLength = count($genreIds);
 
 
   <script>
-    // 選択された要素のcssを変える
+    // 最初に画面を表示したときにスレッドを描画する処理
+    first_output();
+
+    // ニ回目にスレッドを描画する処理
     function isClicked(e, obj, num) {
+      // onClickが2回押すのを防ぐ
       e.stopPropagation();
-      document.getElementById(num).classList.add("genre-active");
+      // 選択したジャンルの「genre_id」を「POST」でこの画面に送信
+      sendPost("", "genre_id", num);
+      // sendPost("http://jolly-ohita-1184.daa.jp/FuJi/chat.php", "room_id", "0000002");
 
-      let thread_element = document.getElementById('thread');
-
-      // for(let i = 0; i < <?php echo $genreLength; ?>; i++) { 
-      //   // 新しいhtml要素を作成
-      //   let new_section = document.createElement('section');
-      //   let new_article = document.createElement('article');
-      //   let new_div = document.createElement('div');
-      //   div.classList.add("info");
-      //   let new_h2 = document.createElement('h2');
-      //   let new_time = document.createElement('time');
-      //   let new_p = document.createElement('p');
-
-      //   // 中身を追加
-      //   new_h2.textContent = '<?php echo "ユーザー名"; ?>';
-      //   new_time.textContent = '<?php echo date('Y年m月d日 H:i', strtotime("2019-03-20 23:22:47"));  ?>';
-      //   new_p.textContent = '<?php echo "本文"; ?>';
-      //   // htmlに追加
-      //   thread_element.appendChild(new_section);
-      //   new_section.appendChild(new_article);
-      //   new_article.appendChild(new_div);
-      //   new_div.appendChild(new_h2);
-      //   new_div.appendChild(new_time);
-      //   new_article.appendChild(new_p);
-      // }
-      // 新しいhtml要素を作成
-      let new_section = document.createElement('section');
-      let new_article = document.createElement('article');
-      let new_div = document.createElement('div');
-      div.classList.add("info");
-      let new_h2 = document.createElement('h2');
-      let new_time = document.createElement('time');
-      let new_p = document.createElement('p');
-
-      // 中身を追加
-      new_h2.textContent = '<?php echo "ユーザー名"; ?>';
-      new_time.textContent = '<?php echo date('Y年m月d日 H:i', strtotime("2019-03-20 23:22:47"));  ?>';
-      new_p.textContent = '<?php echo "本文"; ?>';
-      // htmlに追加
-      thread_element.appendChild(new_section);
-      new_section.appendChild(new_article);
-      new_article.appendChild(new_div);
-      new_div.appendChild(new_h2);
-      new_div.appendChild(new_time);
-      new_article.appendChild(new_p);
-
-
+      output(num);
     }
 
-    // function clickGenre(num) {
-    //   // classList.remove("genre-active");
-    //   // alert("このボタンのIDは" + num + "です。");
-    //   document.getElementById(num).classList.add("genre-active");
+    // スレッドからチャット画面に遷移する処理
+    function goChat(str) {
+      // onClickが2回押すのを防ぐ
+      // e.stopPropagation();
+      // 選択したジャンルの「genre_id」を「POST」でこの画面に送信
+      let form = document.createElement('form');
+      let request = document.createElement('input');
+      form.method = 'POST';
+      form.action = "http://jolly-ohita-1184.daa.jp/FuJi/chat.php";
+      request.type = 'hidden';
+      request.name = "room_id";
+      request.value = str;
+      form.appendChild(request);
+      document.body.appendChild(form);
+      form.submit();
+      // alert(str);
+    }
 
-    //   let thread_element = document.getElementById('thread');
+    // formの処理
+    function sendPost(act, name, num) {
+      let form = document.createElement('form');
+      let request = document.createElement('input');
+      form.method = 'POST';
+      form.action = act;
+      request.type = 'hidden';
+      request.name = name;
+      request.value = num;
+      form.appendChild(request);
+      document.body.appendChild(form);
+      form.submit();
+    }
 
-    //   // 新しいHTML要素を作成
-    //   let new_element = document.createElement('p');
-    //   new_element.textContent = 'success';
+    // 最初に画面を表示したときにスレッドを描画する処理
+    function first_output() {
+      output(
+        <?php
+        if (!empty($_POST['genre_id'])) {
+          echo $_POST['genre_id'];
+        } else {
+          echo 1;
+        }
+        ?>
+      );
+    }
 
-    //   // 指定した要素の中の末尾に挿入
-    //   thread_element.appendChild(new_element);
-    // }
-
-    // let divElement = document.getElementById("thread");
-    // divElement.scrollTop = 1000;
+    // スレッドを描画する処理（numは最初の描画に必要）
+    function output(num) {
+      <?php
+      // 「post」に値があればそれを、無ければ「１」（初期値）を使う
+      if (!empty($_POST['genre_id'])) {
+        $threads = $dbmng->getThreadList($_POST['genre_id']);
+      } else {
+        $threads = $dbmng->getThreadList("1");
+      }
+      // データベースから対応するスレッド一覧を取得し、json形式に変換
+      $i = 0;
+      foreach ($threads as $row) {
+        $threadIds[$i] = $row['room_id'];
+        $threadNames[$i] = $row['room_name'];
+        $threadDetails[$i] = $row['detail'];
+        $i++;
+      }
+      $js_threadIds = json_encode($threadIds);
+      $js_threadNames = json_encode($threadNames);
+      $js_threadDetails = json_encode($threadDetails);
+      ?>
+      // 配列をphp→jsに代入
+      let arr_threadIds = <?php echo $js_threadIds; ?>;
+      let arr_threadNames = <?php echo $js_threadNames; ?>;
+      let arr_threadDetails = <?php echo $js_threadDetails; ?>;
+      // 選択された要素のcssを変える
+      document.getElementById(num).classList.add("genre-active");
+      // スレッドを挿入するタグの「id」を取得
+      let thread_element = document.getElementById('thread');
+      // スレッド一覧を画面から削除する
+      while (thread_element.lastChild) {
+        thread_element.removeChild(thread_element.lastChild);
+      }
+      // ジャンル数を取得
+      let len = arr_threadNames.length;
+      for (let i = 0; i < len; i++) {
+        // console.log("goChat(e,obj,\"" + arr_threadIds[i] + "\")");
+        // 新しいhtml要素を作成
+        let new_button = document.createElement('button');
+        let new_section = document.createElement('section');
+        let new_article = document.createElement('article');
+        let new_div = document.createElement('div');
+        let new_h2 = document.createElement('h2');
+        let new_p = document.createElement('p');
+        // new_a.href = "";
+        new_button.id = arr_threadIds[i];
+        new_button.onclick = function() {
+          goChat(arr_threadIds[i]);
+        };
+        // new_button.style.textDecoration = 'none';
+        new_div.classList.add("info");
+        // 中身を追加
+        new_h2.textContent = arr_threadNames[i];
+        new_p.textContent = arr_threadDetails[i];
+        // htmlに追加
+        thread_element.appendChild(new_button);
+        new_button.appendChild(new_section);
+        new_section.appendChild(new_article);
+        new_article.appendChild(new_div);
+        new_article.appendChild(new_p);
+        new_div.appendChild(new_h2);
+      }
+    }
   </script>
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
