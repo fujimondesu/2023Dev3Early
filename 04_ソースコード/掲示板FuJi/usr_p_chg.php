783ac1be2b2e5b47b0747bc805c88d945e48ef32
yuksaki72 <109458803+yuksaki72@users.noreply.github.com>
2023-07-04 12:28:02 +0900

    修正

--- 04_ソースコード/掲示板FuJi/usr_p_chg.php
+++ 04_ソースコード/掲示板FuJi/usr_p_chg.php
@@ -3,8 +3,8 @@ session_start();
 require_once './DBManager.php';
 $dbmng = new DBManager();
 // 指定したユーザーのパスワードを更新
-$userPass = $dbmng->updatePass($_POST['user_id'],$_SESSION['input_pass']);
-$userName = $dbmng->updateName($_POST['user_id'],$_SESSION['input_user_name']);
+$userPass = $dbmng->updatePass($_SESSION['user_id'],$_SESSION['input_pass']);
+$userName = $dbmng->updateName($_SESSION['user_id'],$_SESSION['input_user_name']);
 
 header('Location: p_chg_com.html');
 
