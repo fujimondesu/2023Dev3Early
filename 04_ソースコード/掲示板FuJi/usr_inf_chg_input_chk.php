d4cb7e8663a90002d4a921b6182b82c390ab31f1
yuksaki72 <109458803+yuksaki72@users.noreply.github.com>
2023-07-04 11:05:36 +0900

    パスワード一致チェック処理追加

--- 04_ソースコード/掲示板FuJi/usr_inf_chg_input_chk.php
+++ 04_ソースコード/掲示板FuJi/usr_inf_chg_input_chk.php
@@ -1,8 +1,11 @@
 <?php
 session_start();
 // メールアドレスとパスワードが入力されているか
-if(empty($_POST['name']) || empty($_POST['pass'])) {
+if(empty($_POST['name']) || empty($_POST['pass1']) || empty($_POST['pass2'])) {
     $_SESSION['error'] = "ユーザー名またはパスワードが入力されていません。";
+    header('Location: usr_inf_chg.php');
+}else if($_POST['pass1'] != $_POST['pass2']){
+    $_SESSION['error'] = "パスワードが一致していません。";
     header('Location: usr_inf_chg.php');
 }else{
     // エラー文を消去
