ad6db3c0c0f2bade55d1ea40fde4efa5bc4ce28c
yuksaki72 <109458803+yuksaki72@users.noreply.github.com>
2023-07-04 11:11:12 +0900

    ユーザー名重複チェック処理追加

--- 04_ソースコード/掲示板FuJi/usr_inf_chg_input_chk.php
+++ 04_ソースコード/掲示板FuJi/usr_inf_chg_input_chk.php
@@ -1,8 +1,14 @@
 <?php
 session_start();
-// メールアドレスとパスワードが入力されているか
+require_once './DBManager.php';
+$dbmng = new DBManager();
+$userNameDouble = $dbmng->userDoubleCheck($_POST['name']);
+// ユーザー名とパスワードが入力されているか
 if(empty($_POST['name']) || empty($_POST['pass1']) || empty($_POST['pass2'])) {
     $_SESSION['error'] = "ユーザー名またはパスワードが入力されていません。";
+    header('Location: usr_inf_chg.php');
+}else if(count($userNameDouble) >= 1){
+    $_SESSION['error'] = "ユーザー名が重複しています。";
     header('Location: usr_inf_chg.php');
 }else if($_POST['pass1'] != $_POST['pass2']){
     $_SESSION['error'] = "パスワードが一致していません。";
