64156a7602250562fac41fa7a4237b0b69519671
yuksaki72 <109458803+yuksaki72@users.noreply.github.com>
2023-07-07 12:10:00 +0900

    ユーザー登録処理修正

--- 04_ソースコード/掲示板FuJi/usr_reg.php
+++ 04_ソースコード/掲示板FuJi/usr_reg.php
@@ -2,7 +2,7 @@
 session_start();
 require_once './DBManager.php';
 $dbmng = new DBManager();
-$user = $dbmng->userRegist($_POST['user_name'],$_POST['mail'],$_POST['pass']);
+$user = $dbmng->userRegist($_SESSION ['name'],$_SESSION ['mail'],$_SESSION ['pass']);
 
-header('Location: su_com.html');
+header('Location:s_up_com.html');
 ?>
