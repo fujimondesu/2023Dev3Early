5286a7dc3871234d6ab4c0038d8d6d874db557ad
yuksaki72 <109458803+yuksaki72@users.noreply.github.com>
2023-07-12 09:41:13 +0900

    処理追加

--- 04_ソースコード/掲示板FuJi/p_chk.php
+++ 04_ソースコード/掲示板FuJi/p_chk.php
@@ -4,7 +4,7 @@ require_once './DBManager.php';
 $dbmng = new DBManager();
 
 // パスワード未入力チェック
-if(empty($_POST['pass'])) {
+if(empty($_POST['pass1']) || empty($_POST['pass2'])) {
     $_SESSION['error'] = "パスワードが入力されていません。";
     header('Location: p_chg_input.php');
 }else{
