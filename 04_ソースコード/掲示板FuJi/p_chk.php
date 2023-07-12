b39ce9b32a375b3458a08857b056461adede9f20
shingo [ <115526343+nakashima0918@users.noreply.github.com>
2023-07-12 10:01:04 +0900

    Revert "遷移ファイル名変更"
    This reverts commit b090e3cc6f339be581cfdbd5ba348dd39cf94634.

--- 04_ソースコード/掲示板FuJi/p_chk.php
+++ 04_ソースコード/掲示板FuJi/p_chk.php
@@ -6,9 +6,9 @@ $dbmng = new DBManager();
 // パスワード未入力チェック
 if(empty($_POST['pass1']) || empty($_POST['pass2'])) {
     $_SESSION['error'] = "パスワードが入力されていません。";
-    header('Location: p_chg_input.php');
+    header('Location: p_chg_input.html');
 }else{
-    header('Location: p_chg_chk.php');
+    header('Location: p_chg_chk.html');
 }
 
 ?>
